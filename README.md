cea601e42daf4fe1d64290f8389d609954e6d4a9
Eddie Chen <yuchuan1@gmail.com>
2015-11-03 16:01:17 +0800

    fix OS X issue, add back accidentally deleted line

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # mp3length
 
 [![Join the chat at https://gitter.im/yuchuan1/mp3length](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/yuchuan1/mp3length?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-Get the length of mp3 files utilizing OS built in functions
 
-	Usage:
-	
-	var mp3Length = require('mp3Length');
-	mp3Length('test.mp3', function (err, length) {
-		console.log('The estimated length test.mp3 is ' + length + ' secs');
-	})
+Get the length of mp3 files utilizing OS built in functions for windows and OSX
+
+## Install
+
+```
+$ npm install --save mp3length
+```
+## Usage
+
+```javascript
+var mp3Length = require('mp3Length');
+mp3Length('test.mp3', function (err, length) {
+  console.log('The estimated length test.mp3 is ' + length + ' secs');
+});
+
+
+```
+
+## API
+
+## mp3Length(filepath, callback) 
+
+### filepath
+
+Type: `string`
+
+Path to the file
+
+### callback(error, length)
+
+Type: `function`
+
+Callback to be called once mp3 length (in seconds) is retrieved
--- index.js
+++ index.js
@@ -10,20 +10,20 @@ module.exports = function mp3Length(mp3file, callback) {
   fs.stat(mp3file, function (err, stats) {
     if (err) {
       callback(err);
-    }
-    else if (!stats.isFile()) {
+    } else if (!stats.isFile()) {
       var error = new Error();
       error.message = 'The entry ' + mp3file + 'is not a file!';
       callback(error);
-    }
-    else {
+    } else {
       var mp3Path = path.parse(mp3file);
       if (process.platform === 'win32') {
         var getMp3Length = spawnSync('cscript', ['/nologo', 'lib/mp3length.vbs', mp3Path.dir, mp3Path.base]),
           result = getMp3Length.stdout.toString(),
           mp3Duration = moment.duration(result);
         callback(null, mp3Duration.asSeconds());
-      } else if (process.platform === 'darwin') {       
+      } else if (process.platform === 'darwin') {
+        var shellCommand = 'afinfo ' + mp3Path.dir + '/' + mp3Path.base + ' |grep \'estimated duration:\'',
+          getMp3Length = spawnSync('afinfo', [mp3Path.dir + '/' + mp3Path.base]);
         var results = getMp3Length.stdout.toString().split('\n'),
           result = null;
         for (var i = 0; i < results.length; i++) {
